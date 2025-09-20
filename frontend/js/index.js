08102844a39b9024a3e72208337419b23b683bd2
Sofiane Sahraoui <sofiane.sahraoui17@gmail.com>
2025-09-20 17:28:55 +0200

    Started section3

---
+++ frontend/js/index.js
@@ -0,0 +1,6 @@
+// Animation de la section 2
+ScrollReveal().reveal('.h2-section2',{ delay: 500 });
+ScrollReveal().reveal('.p-section2', { delay: 1000 });
+ScrollReveal().reveal('.btn-section2', { delay: 1500 });
+
+// Animation de la section 3
