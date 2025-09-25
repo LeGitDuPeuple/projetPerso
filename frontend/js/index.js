98b2332b8f16dbc452544f770accbd4a34f48879
Sofiane Sahraoui <sofiane.sahraoui17@gmail.com>
2025-09-25 16:36:26 +0200

    section 3 fini

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,39 +1,48 @@
-// Animation de la section 2
+// Animation section 2
 ScrollReveal().reveal('.h2-section2',{ delay: 500 });
 ScrollReveal().reveal('.p-section2', { delay: 1000 });
 ScrollReveal().reveal('.btn-section2', { delay: 1500 });
 
+// Animation section 3
+ScrollReveal().reveal(".h3-section3", {delay: 500});
+
 // Variables
-const url ="http://localhost:3000/users";
+const url = "http://localhost:3000/users";
 const div = document.getElementById("users");
 
-//section 3
+// Section 3
+async function getUsers() {
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
 
-async function getUsers(){
-    fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data); 
+        data.forEach(user => {
+            let html = `
+            <article class="users-id">
+                <img src="${user.img}" alt="image du Post">
+                <h3 class="h3-user">${user.user}</h3>
+                <p>${user.poste}</p>
+                <div class="social-icons">
+                    <span><i class="fa-brands fa-instagram"></i></span>
+                    <span><i class="fa-brands fa-twitter"></i></span>
+                    <span><i class="fa-brands fa-facebook"></i></span>
+                </div>
+            </article>`;
+            div.innerHTML += html;
+        });
 
-    //     // Affiche tout les post
-      
-       for (let i = 0; i < data.length; i++) {
-    let html = `   
-        <article class="users-id">
-        <img src="${data[i].img}" alt="image du Post">
-        <h3 class="h3-user">${data[i].user}</h3>
-          <p>${data[i].poste}</p>
-          <div class="social-icons">
-  <span><i class="fa-brands fa-instagram"></i></span>
-  <span><i class="fa-brands fa-twitter"></i></span>
-  <span><i class="fa-brands fa-facebook"></i></span>
-</div>
+        // Animation des cartes après insertion
+        ScrollReveal().reveal('.users-id', {
+            delay: 200,
+            interval: 200,
+            distance: '50px',
+            origin: 'bottom',
+            opacity: 0
+        });
 
-        </article>`;
-    div.innerHTML += html;
+    } catch (error) {
+        console.error("Erreur lors de la récupération des users :", error);
+    }
 }
 
-    })
-    .catch(error => console.error("Erreur lors de la récupération des users :", error));
-}
 getUsers();
