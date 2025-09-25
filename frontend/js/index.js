489040bc48c4dd9a4615b3bf6bc7b64d4d4bc561
Sofiane Sahraoui <sofiane.sahraoui17@gmail.com>
2025-09-25 12:25:20 +0200

    projet

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -23,6 +23,12 @@ async function getUsers(){
         <img src="${data[i].img}" alt="image du Post">
         <h3 class="h3-user">${data[i].user}</h3>
           <p>${data[i].poste}</p>
+          <div class="social-icons">
+  <span><i class="fa-brands fa-instagram"></i></span>
+  <span><i class="fa-brands fa-twitter"></i></span>
+  <span><i class="fa-brands fa-facebook"></i></span>
+</div>
+
         </article>`;
     div.innerHTML += html;
 }
